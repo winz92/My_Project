4e93b86aead29cfde3941b0d6833dae985d75971
winz92 <vincentvargas6827@hotmail.com>
2015-10-14 10:44:05 +0200

    My initial commit message

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ActionResult Index()
+        {
+            logger.Error(Encrypt256("test"));
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        private const string AesIV256 = @"!QAZ2WSX#EDC4RFV";
+        private const string AesKey256 = @"5TGB&YHN7UJM(IK<5TGB&YHN7UJM(IK<";
+
+
+        private string Encrypt256(string text)
+        {
+            // AesCryptoServiceProvider
+            System.Security.Cryptography.AesCryptoServiceProvider aes = new System.Security.Cryptography.AesCryptoServiceProvider();
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.IV = System.Text.Encoding.UTF8.GetBytes(AesIV256);
+            aes.Key = System.Text.Encoding.UTF8.GetBytes(AesKey256);
+            aes.Mode = System.Security.Cryptography.CipherMode.CBC;
+            aes.Padding = System.Security.Cryptography.PaddingMode.PKCS7;
+
+            // Convert string to byte array
+            byte[] src = System.Text.Encoding.Unicode.GetBytes(text);
+
+            // encryption
+            using (System.Security.Cryptography.ICryptoTransform encrypt = aes.CreateEncryptor())
+            {
+                byte[] dest = encrypt.TransformFinalBlock(src, 0, src.Length);
+
+                // Convert byte array to Base64 strings
+                return Convert.ToBase64String(dest);
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Web.config", Watch = true)] 
+namespace WebApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
